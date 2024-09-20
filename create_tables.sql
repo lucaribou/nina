8b229d20bb843827fb2b344f05c358c2098a70d1
Lucas SAISSI <lucas.saissi@mgss2i.com>
2024-09-20 14:53:11 +0200

    ajout colonne

--- create_tables.sql
+++ create_tables.sql
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS products (
     product_id INT PRIMARY KEY AUTO_INCREMENT,
     product_name VARCHAR(100) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
+    test DECIMAL(10, 2) NOT NULL,
     stock INT DEFAULT 0
 );
 
