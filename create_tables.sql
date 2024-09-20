51d5a26ea7f789fe79366a31c4694804f94f2b31
Lucas SAISSI <lucas.saissi@mgss2i.com>
2024-09-20 14:50:51 +0200

    add table products

--- create_tables.sql
+++ create_tables.sql
@@ -14,3 +14,13 @@ CREATE TABLE IF NOT EXISTS products (
     price DECIMAL(10, 2) NOT NULL,
     stock INT DEFAULT 0
 );
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    product_id INT,
+    quantity INT NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
